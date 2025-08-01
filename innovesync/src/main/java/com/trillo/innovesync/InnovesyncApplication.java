a33c95b2d1cdc30aeb4bbb5142f90a4234e89886
Sasindi Bandara <sasindi.bandara2003@gmail.com>
2025-08-01 15:09:02 +0530

    Springboot project intiate

---
+++ innovesync/src/main/java/com/trillo/innovesync/InnovesyncApplication.java
@@ -0,0 +1,13 @@
+package com.trillo.innovesync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovesyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovesyncApplication.class, args);
+	}
+
+}
---
+++ innovesync/src/test/java/com/trillo/innovesync/InnovesyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trillo.innovesync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovesyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
